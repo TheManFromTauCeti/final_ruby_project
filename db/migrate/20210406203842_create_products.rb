28b84c44cd240b2e34cef3a3d5b26b5544f08fa4
Adam Lukowski <adamsowalukowski@gmail.com>
2021-04-06 15:39:23 -0500

    Generates Product model

---
+++ db/migrate/20210406203842_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+      t.decimal :price
+      t.boolean :in_stock
+      t.integer :stock_quantity
+
+      t.timestamps
+    end
+  end
+end
