6dcbf5ec6f25c5544294694320072c61eeb3712a
Adam Lukowski <adamsowalukowski@gmail.com>
2021-04-06 16:07:41 -0500

    Adds Active Admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,60 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# AdminUser.create!(
+#   email:                 "adam@lukowski.com",
+#   password:              "password",
+#   password_confirmation: "password"
+#                   ) if Rails.env.development?
+
+# require "uri"
+
+# Category.delete_all
+# Product.delete_all
+
+# categories = [
+#   "Espresso Roast",
+#   "Light Roast",
+#   "Medium Roast",
+#   "Dark Roast",
+#   "Decaf"
+# ]
+
+# gear = Category.create(name: "Brew Gear")
+
+# # Rails.logger.debug "Creating #{gear.name}"
+
+# # downloaded_image = URI.open("https://source.unsplash.com/600x600/?kettle")
+# # gear.image.attach(io: downloaded_image, filename: "m-#{gear.name}.jpg")
+
+# tea = Category.create(name: "Loose Leaf Tea")
+
+# # Rails.logger.debug "Creating #{tea.name}"
+
+# # downloaded_image = URI.open("https://source.unsplash.com/600x600/?tea")
+# # tea.image.attach(io: downloaded_image, filename: "m-#{tea.name}.jpg")
+
+# categories.each do |category|
+#   c = Category.create(name: category.to_s)
+
+#   # downloaded_image = URI.open("https://source.unsplash.com/600x600/?coffee")
+#   # c.image.attach(io: downloaded_image, filename: "m-#{c.name}.jpg")
+#   # sleep(0.5)
+
+#   5.times do
+#     p = Product.create(
+#       name:           Faker::Coffee.unique.blend_name,
+#       price:          Faker::Commerce.price(range: 15..20.0),
+#       in_stock:       true,
+#       stock_quantity: Faker::Number.between(from: 1, to: 20),
+#       description:    "From #{Faker::Coffee.origin}, " \
+#                       "this coffee has notes of #{Faker::Coffee.unique.notes}."
+#     )
+
+#     yolo = ProductCategory.create(
+#       product:  p,
+#       category: c
+#     )
+
+#     # downloaded_image = URI.open("https://source.unsplash.com/600x600/?coffee")
+#     # p.image.attach(io: downloaded_image, filename: "m-#{p.name}.jpg")
+#     # sleep(0.5)
+#   end
+# end
