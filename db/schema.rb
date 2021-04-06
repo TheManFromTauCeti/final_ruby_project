7d31fa56c30a79465ca9a40a3b076b418f5b6f38
Adam Lukowski <adamsowalukowski@gmail.com>
2021-04-06 15:40:34 -0500

    Generates Category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_06_203842) do
+ActiveRecord::Schema.define(version: 2021_04_06_204010) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
