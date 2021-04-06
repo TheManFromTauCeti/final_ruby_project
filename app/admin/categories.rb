8b54994255dca5e3e63c330ee2f2a7b8d4f62f4c
Adam Lukowski <adamsowalukowski@gmail.com>
2021-04-06 16:24:52 -0500

    Modifies categories and products in /admin/

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -1,3 +1,11 @@
 ActiveAdmin.register Category do
-  permit_params :name
+  permit_params :name, :image
+  form do |f|
+    f.semantic_errors # shows errors on :base
+    f.inputs # builds an input field for every attribute
+    f.inputs do
+      f.input :image, as: :file, hint: f.object.image.present? ? image_tag(f.object.image) : ""
+    end
+    f.actions # adds the "Submit" and "Cancel" buttons
+  end
 end
