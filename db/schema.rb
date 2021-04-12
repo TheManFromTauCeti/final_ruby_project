d18b016c5f083bce8b8eb3d36d44af730ce4a6d8
Adam Lukowski <adamsowalukowski@gmail.com>
2021-04-12 13:39:50 -0500

    Generates Order model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_12_183600) do
+ActiveRecord::Schema.define(version: 2021_04_12_183828) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -82,6 +82,13 @@ ActiveRecord::Schema.define(version: 2021_04_12_183600) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "orders", force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["customer_id"], name: "index_orders_on_customer_id"
+  end
+
   create_table "product_categories", force: :cascade do |t|
     t.integer "category_id", null: false
     t.integer "product_id", null: false
@@ -103,6 +110,7 @@ ActiveRecord::Schema.define(version: 2021_04_12_183600) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "orders", "customers"
   add_foreign_key "product_categories", "categories"
   add_foreign_key "product_categories", "products"
 end
