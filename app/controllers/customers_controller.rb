780f964b2218844f2ee89e3a2799cf9f1fd51d87
Adam Lukowski <adamsowalukowski@gmail.com>
2021-04-13 10:54:33 -0500

    Adds Create Account

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -6,6 +6,30 @@ class CustomersController < ApplicationController
   end
 
   def create
+    @customer = Customer.new(
+      first_name:            params[:first_name],
+      last_name:             params[:last_name],
+      email:                 params[:email],
+      password:              params[:password],
+      password_confirmation: params[:password_confirmation]
+    )
+
+    # Save will return false if any of our validations fail.
+    @worked = @customer.save
+
+    if @worked
+      puts "customer was saved to the database customers table."
+      puts @customer.inspect
+    else
+      puts "There was an error saving customer to the database."
+
+      # We can loop through all the validation errors.
+      @customer.errors.messages.each do |column, errors|
+        errors.each do |error|
+          puts "The #{column} property #{error}."
+        end
+      end
+    end
   end
 
   def new
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,6 @@
 class Customer < ApplicationRecord
+  validates :email, uniqueness: true
+  validates :first_name, :last_name, :email, :password_digest, presence: true
   has_secure_password
   has_secure_password :recovery_password, validations: false
 end
